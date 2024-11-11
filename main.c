221ce9d8627ce79ffe8e8458a7d729ee5feb9834
CRN91 <ak824@exeter.ac.uk>
2024-11-11 15:09:58 +0000

    Checks file exists, writes to report

--- main.c
+++ main.c
@@ -2,14 +2,31 @@
 
 int main()
 {
-  printf("Hello World\n");
-  
-  FILE *config;
+  FILE *config, *report;
   float mean_interarrival, mean_service;
   int delays_required;
+  
+  /* Open files */
   config = fopen("config.in","r");
+  report = fopen("report.txt","w");
+  
+  /* Check files exist */
+  if(config == NULL || report == NULL)
+  {
+    printf("File read error!");
+    exit(1);
+  }  
+  
   fscanf(config, "%f %f %d", &mean_interarrival, &mean_service, &delays_required);
+  fclose(config);
   printf("Mean interarrival time: %f\nMean service time: %f\nNumber of required delays: %d\n",mean_interarrival, mean_service, delays_required);
+  
+  /* Write heading of report */
+  fprintf(report, "Test write");
+  fclose(report);
+  
+  /* Simulation Loop */
+  
   
   return 0;
 }
