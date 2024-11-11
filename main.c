bcc4d462ac56d1dec56d05df88598d0b18e390b3
CRN91 <ak824@exeter.ac.uk>
2024-11-11 17:12:10 +0000

    Uniformly distributed and exponentially distributed random variate generator functions

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <math.h>
 
 /* Initialises the sim */
@@ -38,28 +40,34 @@ void arrive()
 }
 
 /* Generate random uniformly distribute variate between 0 and 1 */
-float random_uniform()
-{
-
+float gen_rand_uniform()
+{  
+  // Generates variate between 0 and 1
+  float random_variate = (float)rand() / RAND_MAX;
+   
+  return random_variate;
 }
 
 /* Generate random exponentially distributed variate between 0 and 1 */
-float random_exponential(float uniform)
+float gen_rand_exponential(float uniform, float beta)
 {
-
+  return -1 * beta * log(uniform);
 } 
 
 int main()
 {
+  // Seed random number generator with current time once at start of program
+  srand(time(NULL));
+
   FILE *config, *report;
-  float mean_interarrival, mean_service;
+  float mean_interarrival, mean_service, uniform_rand;
   int delays_required;
   
-  /* Open files */
+  // Open files
   config = fopen("config.in","r");
   report = fopen("report.txt","w");
   
-  /* Check files exist */
+  // Check files exist
   if(config == NULL || report == NULL)
   {
     printf("Error! File not read.");
@@ -69,22 +77,28 @@ int main()
   fscanf(config, "%f %f %d", &mean_interarrival, &mean_service, &delays_required);
   fclose(config);
   printf("Mean interarrival time: %f\nMean service time: %f\nNumber of required delays: %d\n",mean_interarrival, mean_service, delays_required);
+  for (int i = 0; i < 2; i++){
+    uniform_rand = gen_rand_uniform();
+    printf("uniform random variate: %f\n", uniform_rand);
+    printf("exponential random variate: %f\n", gen_rand_exponential(uniform_rand, mean_interarrival));
+  }
   
-  /* Write heading of report */
+  // Write heading of report
   fprintf(report, "Test write");
   
-  /* Simulation Loop */
+  // Simulation Loop
   int delays;
   while (delays < delays_required)
   {
     delays = 1000;
-    /* Timing event to determine next event */
+    // Timing event to determine next event
     
-    /* Update Time Average Statistical Counters */
+    // Update Time Average Statistical Counters
     
-    /* Call correct event function */
+    // Call correct event function
     int event_type = 1;
-    switch (event_type){
+    switch (event_type)
+    {
       case 1:
         break;
       case 2:
@@ -92,9 +106,10 @@ int main()
       default:
         printf("Error! Event type incorrect.");
         exit(1);
+    }
   }
   
-  /* Call the report writing function */
+  // Call the report writing function
   fclose(report);
   
   return 0;
