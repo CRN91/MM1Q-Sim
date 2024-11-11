5dd0c312b410e461bacfc39123be65f18d1e66cc
CRN91 <ak824@exeter.ac.uk>
2024-11-11 14:49:13 +0000

    Initial commit from within Eclipse IDE, C code read input parameters from file.

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-  printf("Hello World");
+  printf("Hello World\n");
+  
+  FILE *config;
+  float mean_interarrival, mean_service;
+  int delays_required;
+  config = fopen("config.in","r");
+  fscanf(config, "%f %f %d", &mean_interarrival, &mean_service, &delays_required);
+  printf("Mean interarrival time: %f\nMean service time: %f\nNumber of required delays: %d\n",mean_interarrival, mean_service, delays_required);
+  
   return 0;
 }
